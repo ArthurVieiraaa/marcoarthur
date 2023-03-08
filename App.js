b2210e0e14afa802e110075c0e8ee209a91c5423
ArthurVieiraaa <avieirabruske@gmail.com>
2023-03-08 16:51:19 -0300

    atividade marco fausto

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
+function Feed() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Inicio!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Contato() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Contato!</Text>
+    </View>
+  );
+}
+
+function About() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sobre!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function Apps() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        tabBarActiveTintColor: '#10a7da',
+      }}
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Sobre"
+        component={About}
+        options={{
+          tabBarLabel: 'Sobre',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contato"
+        component={Contato}
+        options={{
+          tabBarLabel: 'Contato',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-multiple" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
